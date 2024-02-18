95d3f482cde430013502c48406c964c62eccd1fb
simonetignino <64482761+simonetignino@users.noreply.github.com>
2024-02-18 12:23:34 +0100

    Caricamento esercizio fine seconda settimana

---
+++ S2P2-scaricabile.js
@@ -0,0 +1,80 @@
+// Carrello e sconti particolari
+
+/*
+Oggi il tuo compito è creare la logica per un sito di e-commerce che deve supportare sconti extra per utenti speciali.
+A partire da una lista di prezzi, un utente e un costo di spedizione l'algoritmo deve determinare il costo totale del carrello.
+ATTENZIONE! Gli argomenti di questa settimana sono cruciali per lo svolgimento di un lavoro di un developer (il 90% del dati che maneggerai saranno array di oggetti!!) quindi 
+assicurati di COMPRENDERE la logica. Se ti trovi in difficolta', scrivi ad un membro del teaching staff! :) 
+
+Se l'utente ha la proprietà "isAmbassador" con valore true, il carrello deve venire scontato del 30%, PRIMA di calcolare la spedizione (anche gli utenti speciali pagano le spedizioni).
+Se l'utente ha la proprietà "isAmbassador" con valore false, il carrello NON deve venire scontato.
+In entrambi i casi, la spedizione è gratuita per ogni carrello con costo superiore a 100. Altrimenti, aggiungi il valore fornito per coprire il costo della spedizione.
+
+In basso troverai degli esempi di utenti, una lista prezzi e un costo fisso di spedizione.
+Crea un array di utenti (usando .push) e stampa, per ogni utente (quindi con un loop) la frase "NOMEUTENTE COGNOMEUTENTE e' / non e' un ambassador" basandoti sui dati contenuti nell'oggetto. 
+ES. L'utente Marco Rossi e' un ambassador, quindi la frase dovrebbe essere "Marco Rossi e' un ambassador"
+Infine, crea un SECONDO array in cui inserirai SOLO gli ambassador.
+*/
+
+const marco = {
+  name: "Marco",
+  lastName: "Rossi",
+  isAmbassador: true,
+}
+
+const paul = {
+  name: "Paul",
+  lastName: "Flynn",
+  isAmbassador: false,
+}
+
+const amy = {
+  name: "Amy",
+  lastName: "Reed",
+  isAmbassador: false,
+}
+
+let prices = [34, 5, 2]
+const shippingCost = 50
+let utenteCheEffettuaLAcquisto = amy //cambia il valore qui per provare se il tuo algoritmo funziona!
+
+//calcolo il costo toale del carrello e lo inserisco dentro una variabile
+let totalCost = 0;
+for(let i = 0; i<prices.length; i++){
+  totalCost += prices[i];
+}
+
+//console.log(totalCost);
+
+//creo l'array di users
+let users = [];
+users.push(marco, paul, amy);
+
+//creo l'array di soli ambassador
+let ambassadors = [];
+
+//console.table(users);
+
+//controllo se l'user ha diritto al 30% di sconto 
+if(users[1].isAmbassador){
+  console.log(users[1].name, "è un ambassador, quindi ha il 30% di sconto");
+  totalCost *= 0.7;
+  if(totalCost <= 100){
+    totalCost += 50;
+  }
+//se non è un ambassador e il totalCost è <= 100 aggiungi 50, in caso contrario l'user non paga la spedizione
+} else if (totalCost<=100){
+  totalCost += 50;
+}
+
+console.log("il costo totale del carrello è: " + totalCost);
+
+//ciclo per scorrere l'array di users
+for(let i = 0; i< users.length; i++){
+  if(users[i].isAmbassador){
+    ambassadors.push(users[i]);
+    console.log(users[i].name, users[i].lastName, "è un ambassador");
+  }
+}
+
+console.table(ambassadors);
